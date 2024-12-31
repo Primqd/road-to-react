49dd0bedb73d375da497ed4478417caebe6b00c0
Primqd <franklinvchen@outlook.com>
2024-12-31 00:39:07 -0800

    completed up until 'React Dom'

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,65 @@
 import * as React from 'react';
 
+// npm run dev
 // "bridge" that makes "auto-refresh" possible is react Fast Refresh on React's side and Hot Module Replacement on dev server side
-// jsx enables combining of markup (HTML) and logic (JS)
+// jsx enables combining of markup (HTML) and logic (JS): map allows mapping from js array to html
 
-const welcome = {
-  greeting: "Hey",
-  title: "React"
-};
+// points = popularity
+// don't fit standard js naming conventions
+// key attribute used for efficent rerender: when have to rerender, checks whether item has changed; can efficently exchanged changed item w/ key identifier
+const list = [
+  {
+    title : 'React',
+    url : 'https://reactjs.org/',
+    author : 'Jordan Walke',
+    num_comments : 3,
+    points : 4,
+    objectID : 0,
+  },
+  {
+    title : 'Redux',
+    url : 'https://redus.js.org/',
+    author : 'Dan Abramov, Andrew Clark',
+    num_comments : 2,
+    points : 5,
+    objectID : 1,
+  },
+];
 
-
-function getTitle(title: string) {
-  return welcome.title.concat(title);
+function Search() {
+  // component for the serach bar
+  return(
+    <div>
+      <label htmlFor="test_input">Search: </label>
+      <input type="text" id="test_input" name="test_input"></input>
+    </div>
+  );
 }
 
+// child component of app, leaf component (doesn't render any other components)
+// component similar idea to 'class' in other
+// we use components as elements anywhere else, generate instances of List
+function List() {
+  return (
+    <ul>
+    {list.map((item, index) => {
+      return (
+      <li key = {item.objectID}>
+        <span>
+          <a href = {item.url}>{item.title} </a>
+        </span>
+        <span>{item.author} </span>
+        <span>{item.num_comments} </span>
+        <span>{item.points} </span>
+        <span>{index}</span> {/*index of item in list!*/}
+      </li>
+      );
+    })}
+  </ul>
+  );
+}
+
+// entry point or root component; root of component tree
 function App() {
   // React component (App Component specifically) is just a js function, CamelCase + staring capital (determined by function starting with Capital)
   // specifically function component, most common although other types
@@ -20,23 +67,20 @@ function App() {
 
   // htmlFor reflects attribute in vanilla html, due to impementation detauls of React
   // TSX closer to js than html, so uses camelCase
-  const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8];
 
-  const numItems = nums.map((num) => ( // # nums to jsx objects
-    <li>
-      {num}
-    </li>
-  ));
-
-  return ( // {title} calls VARIABLE title! how TSX differs from pure HTML
+  return (
     <div>
-      <h1>Hello React</h1>
+      <h1>My Hacker Stories</h1>
 
-      <label htmlFor="test_input">Test</label>
-      <input type="text" id="test_input" name="test_input"></input>
-      <ul>{numItems}</ul>
+      <Search />
+
+      <hr /> {/*hr = line break*/}
+
+      <List />
+      <List />
+
     </div>
   );
 }
 
-export default App
+export default App;
